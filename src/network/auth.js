b50b0f4f5b2f61762bd2983a7fa464efc95cc0e7
YoussefAshraf11 <youssefashraf1103@gmail.com>
2025-06-02 19:36:39 +0300

    integration of forget password

--- src/network/auth.js
+++ src/network/auth.js
@@ -5,4 +5,10 @@ const login = async (payload) => await axiosInstance.post('/api/auth/login',payl
 
 const signup = async (payload) => await axiosInstance.post('/api/auth/signup', payload);
 
-export {login, signup};
+const validateUserAndSendOtp = async (payload) => await axiosInstance.post('/api/auth/validateUserAndSendOtp', payload);
+
+const validateOtp = async (payload) => await axiosInstance.post('/api/auth/validate-otp', payload);
+
+const resetPasswordOtp = async (payload) => await axiosInstance.post('/api/auth/reset-password-otp', payload);
+
+export {login, signup, validateUserAndSendOtp, validateOtp, resetPasswordOtp};
